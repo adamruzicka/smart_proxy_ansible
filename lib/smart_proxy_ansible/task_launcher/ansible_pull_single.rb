c3ea4e6987e9e281efa915816d859c5a2718cb33
Adam Ruzicka <aruzicka@redhat.com>
2024-05-28 13:12:26 +0200

    ...

--- lib/smart_proxy_ansible/task_launcher/ansible_pull_single.rb
+++ lib/smart_proxy_ansible/task_launcher/ansible_pull_single.rb
@@ -5,9 +5,41 @@ module Proxy::Ansible
   module TaskLauncher
     class AnsiblePullSingle < Proxy::Dynflow::TaskLauncher::Single
       def launch!(input, id: nil)
-	input['action_input']['script'] = "#!/usr/bin/env -S ansible-playbook -c local -i,localhost\n" + input['action_input']['script']
-	input['action_class'] = 'Proxy::RemoteExecution::Ssh::Actions::RunScript'
-	super(input, id: id)
+	      input['action_input']['script'] = build_script(input)
+	      input['action_class'] = 'Proxy::RemoteExecution::Ssh::Actions::RunScript'
+	      super(input, id: id)
+      end
+
+      private
+
+      def build_script(input)
+        inventory_hash = input['action_input']['ansible_inventory']
+        name = input['action_input']['name']
+        vars = inventory_hash['_meta']['hostvars']
+
+        # TODO: tags
+        # TODO: verbosity
+        # TODO: check mode
+        # TODO?: secrets
+
+        # We need to force local connection. We force it with a parameter passed
+        # to ansible-playbook, but this host variable would override it if left
+        # there.
+        vars[name].delete('ansible_connection')
+        inventory = { 'all' => { 'hosts' => vars }, 'vars' => inventory_hash['all']['vars'] }
+        <<~SCRIPT
+          #!/bin/bash
+          export ANSIBLE_INVENTORY_ENABLED=yaml
+
+          exec ansible-playbook -c local --inventory <(cat | base64 -d <<EOF
+          #{Base64.strict_encode64(JSON.dump(inventory))}
+          EOF
+          ) \
+          <(cat | base64 -d <<EOF
+          #{Base64.strict_encode64(input['action_input']['script'])}
+          EOF
+          )
+        SCRIPT
       end
     end
   end
